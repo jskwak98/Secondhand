4407d2f6a43cf1932eff2da3e0f22d78e362bd4d
Jinseong <jskwak98@naver.com>
2022-11-14 18:01:59 +0900

    added username for confirmation

--- common/serializers.py
+++ common/serializers.py
@@ -4,4 +4,4 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'broker_id')
+        fields = ('id', 'broker_id', 'username')
